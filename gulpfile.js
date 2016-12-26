8ef42b95166e2b8e903110421b65cf400ae33502
gitwsx <1211373436@qq.com>
2016-12-26 10:18:18 +0800

    更改

--- gulpfile.js
+++ gulpfile.js
@@ -1,101 +1,98 @@
+/**
+ * Created by Administrator on 2016/12/12.
+ */
+//npm install gulp -g  先全局安装
+//npm install gulp --save-dev
+//npm install gulp-stylus --save-dev
+//npm install gulp-minify-css --save-dev
+//npm install gulp-uglify --save-dev
+//以上在git上安装
 
-var gulp = require('gulp'),
-	stylus = require('gulp-stylus'),
-	concat = require('gulp-concat'),
-	minifycss = require('gulp-minify-css'),
-	uglify = require('gulp-uglify'),
-	htmlmin = require('gulp-htmlmin'),
-	imagemin = require('gulp-imagemin'),
-	nodemon = require('nodemon'),
-	del = require('del');
+//首先引入gulp模块
+var gulp = require( 'gulp' );
+//引入 gulp-stylus插件
+var stylus = require( 'gulp-stylus' );
+//引入压缩css的文件  运行gulp minifycss
+var minifycss = require( 'gulp-minify-css' );
+//gulp-uglify
+var uglify = require( 'gulp-uglify' );
 
-var browserSync = require('browser-sync').create();
+//npm install browser-sync --save-dev
+var browserSync = require( 'browser-sync' ).create();
+//var serverStart=browserSync.create();
 var reload = browserSync.reload;
 
-//编译stylus
-gulp.task('stylus', function(){
-	return gulp.src('./static/stylus/**/*.styl')
-		.pipe(stylus())
-		.pipe(gulp.dest('./static/css'));
+//npm install gulp-nodemon --save-dev
+var nodemon = require('gulp-nodemon');
+gulp.task('nodemon',function(ab){
+    var ft = false;
+    return nodemon({
+        script:'./server.js'
+    } ).on('start',function(){
+        if(!ft){
+            ab();
+            ft = true;
+        }
+    })
 });
 
-//压缩css
-gulp.task('minifycss', ['stylus'], function(){
-	return gulp.src('./static/css/**/*.css')
-		.pipe(minifycss())
-		.pipe(gulp.dest('./dist/static/css'));
+gulp.task('browserSync',['nodemon'],function(){
+    browserSync.init({
+        proxy:{
+            target:'http://127.0.0.1:16941'
+        },
+        port:9888,
+        open:false,
+        files:['*']
+    })
 });
 
-//压缩html
-gulp.task('htmlmin',function(){
-	// gulp.src(['.views/index.html','./views/login.html'])
-	gulp.src('./views/**/*.html')
-		.pipe(htmlmin())
-		.pipe(gulp.dest('./dist/views'));
-});
+//创建一个编译 stylus 的任务
+gulp.task( 'stylus', function()
+{
+    //获取要编译的文件
+    //指定一个文件
+    //gulp.src('./stylus/index.styl')
+    //指定多个文件
+    //gulp.src(['./stylus/index.styl','./stylus/css.styl'])
+    //指定一个目录下所有（不包含子目录）
+    //gulp.src('./stylus/*.styl')
+    //指定一个目录及所有子目录下的文件
+    return gulp.src( './stylus/**/*.styl' )
+        //执行 stylus 编译
+        .pipe( stylus() )
+        //输出编译后的文件
+        .pipe( gulp.dest( './public/css' ) )
+} );
 
-//压缩js
-gulp.task('uglify',function(){
-	gulp.src('./static/js/**/*.js')
-		.pipe(uglify())
-		.pipe(gulp.dest('./dist/static/js'));
-});
+//压缩css文件
+gulp.task( 'minifycss', [ 'stylus' ], function()
+{//添加前置执行任务stylus
+    return gulp.src( './public/css/**/*.css' )
+        .pipe( minifycss() )
+        .pipe( gulp.dest( './public/mincss' ) )
+} );
+//压缩js文件
+gulp.task( 'uglify', function()
+{//添加前置执行任务stylus
+    return gulp.src( './public/js/**/*.js' )
+        .pipe( uglify() )
+        .pipe( gulp.dest( './public/minjs' ) )
+} );
 
+//创建监听文件变更
+gulp.task( 'watcher', ['browserSync', 'stylus', 'uglify' ], function()
+{
+    gulp.watch( './stylus/**/*.styl', [ 'stylus' ]);
+    gulp.watch( './public/js/**/*.js', [ 'uglify' ]);
 
-//压缩图片
-gulp.task('imagemin',function(){
-	gulp.src('./static/images/**/*.{png,jpg,gif,ico}')
-		.pipe(imagemin())
-		.pipe(gulp.dest('./dist/static/images'));
-});
+    gulp.watch(['./public/css/**/*.css','./public/minjs/**/*.js'] ).on('change',function(){
+        reload();
+    })
+} );
 
-
-//启动服务器
-gulp.task('nodemon', (a)=> {
-	let ft = false;
-	return nodemon({
-		script: 'app.js'
-	}).on('start', ()=> {
-		if (!ft) {
-			a();
-			ft = true;
-		}
-	});
-});
-
-//  proxy 服务器代理
-gulp.task('browser-sync',['nodemon'] , ()=> {
-	browserSync.init({
-	proxy: {
-		target: 'http://127.0.0.1:16081'
-	},
-	files: ['*'],
-	open: false,
-	notify: false,
-	port: 9800
-});
-});
-
-//编译前清除文件
-gulp.task('clean', function(opt){
-	del(['./dist'], opt);
-});
-
-//构建项目
-gulp.task('build', ['stylus', 'minifycss', 'uglify', 'imagemin', 'htmlmin']);
-
-//监听文件变更
-gulp.task('watch', function(){
-	gulp.watch('./static/stylus/**/*.styl', ['stylus']);
-	gulp.watch([
-		'./views/**/*.html',
-		'./static/css/**/*.css',
-		'./static/js/**/*.js',
-		'./static/images/**/*.{png,jpg,ico,gif}}'
-	]).on('change', reload);
-});
-
-//启动任务
-gulp.task('default', ['browser-sync', 'stylus', 'watch'], function(){
-	console.log("gulp default");
-});
+//创建一个default任务
+gulp.task( 'default', function()
+{
+    console.log( 'this default' );
+} );
